f421966845ded8ebbcc2fc05f3219e93950da3da
Steven Lee <stevenyslee@gmail.com>
2018-08-30 11:52:12 -0700

    Setup client

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,10 @@
-
-
+const React = require('react');
+const ReactDOM = require('react-dom');
+ 
+class MyComponent extends React.Component {
+  render() {
+    return (<div>Hello World</div>);
+  }
+}
+ 
+ReactDOM.render(<MyComponent />, document.getElementById('app'));
