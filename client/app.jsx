ca04563e9bf9cbd08c0b881eacf3962895187717
Steven Lee <stevenyslee@gmail.com>
2018-08-30 12:23:20 -0700

    Rename component

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
  
-class MyComponent extends React.Component {
+class App extends React.Component {
   render() {
     return (<div>Hello World</div>);
   }
 }
  
-ReactDOM.render(<MyComponent />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
