d0c82e60188f97c5266b5eb8a2a6f05e3cb3078f
Steven Lee <stevenyslee@gmail.com>
2019-01-06 15:26:56 -0800

    Setup basic component templates

--- client/app.jsx
+++ client/app.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Name from './name.jsx';
-
-const elementsSymbol = [ "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt" ];
+import functionalComponent from './functionalComponent.jsx';
  
 class App extends React.Component {
   render() {
-    return (<div>
-      <form>
-      <label>
-        <input type="text" name="name" />
-      </label>
-      </form>
-      <Name line={"Test1"} />
-      <Name line={"Test2"} />
-    </div>);
+    return (
+      <div>
+        <h1>Hello</h1>
+        <functionalComponent />
+      </div>
+    );
   }
 }
  
---
+++ client/functionalComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function functionalComponent() {
+    return(
+      <div>
+        <h2>Functional Component</h2>
+      </div>
+      )
+    }
+}
+
+export default functionalComponent;
